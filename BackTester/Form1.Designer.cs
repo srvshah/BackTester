c90ac58b75e9eea5435b1f139130300ffb171ffd
Saurav Shah <sauravhshah99@gmail.com>
2020-08-17 13:03:22 +0545

    Add project files.

---
+++ BackTester/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BackTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelWin = new System.Windows.Forms.Label();
+            this.labelLoss = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "WIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "LOSS";
+            // 
+            // labelWin
+            // 
+            this.labelWin.AutoSize = true;
+            this.labelWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWin.Location = new System.Drawing.Point(106, 19);
+            this.labelWin.Name = "labelWin";
+            this.labelWin.Size = new System.Drawing.Size(19, 20);
+            this.labelWin.TabIndex = 2;
+            this.labelWin.Text = "--";
+            // 
+            // labelLoss
+            // 
+            this.labelLoss.AutoSize = true;
+            this.labelLoss.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLoss.Location = new System.Drawing.Point(106, 54);
+            this.labelLoss.Name = "labelLoss";
+            this.labelLoss.Size = new System.Drawing.Size(19, 20);
+            this.labelLoss.TabIndex = 3;
+            this.labelLoss.Text = "--";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(193, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 16);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "play / pause";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 136);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelLoss);
+            this.Controls.Add(this.labelWin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BackTester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelWin;
+        private System.Windows.Forms.Label labelLoss;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BackTester/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BackTester
+{
+    public partial class Form1 : Form
+    {
+        GlobalKeyboardHook gHook;
+        bool running = false;
+        double win, loss = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gHook = new GlobalKeyboardHook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            foreach (Keys key in Enum.GetValues(typeof(Keys)))
+                gHook.HookedKeys.Add(key);
+            running = true;
+            updateStatus();
+        }
+
+        public void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            string input = ((char)e.KeyValue).ToString().ToUpper();
+
+            if (input == Keys.Z.ToString())
+            {
+                win++;
+                labelWin.Text = win.ToString();
+            }
+            else if (input == Keys.X.ToString())
+            {
+                loss++;
+                labelLoss.Text = loss.ToString();
+            }
+            else if (input == Keys.Q.ToString())
+            {
+                win = loss = 0;
+                labelWin.Text = "--";
+                labelLoss.Text = "--";
+            }
+            else if (input == Keys.A.ToString())
+            {
+                if (win > 0)
+                {
+                    win--;
+                    labelWin.Text = win.ToString();
+                }
+            }
+            else if (input == Keys.S.ToString())
+            {
+                if (loss > 0)
+                {
+                    loss--;
+                    labelLoss.Text = loss.ToString();
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            running = !running;
+            updateStatus();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            gHook.unhook();
+        }
+
+        private void updateStatus()
+        {
+            if (running) {
+                gHook.hook();
+                pictureBox1.BackColor = Color.Green;
+                button1.Text = "STOP";
+            }
+            else
+            {
+                gHook.unhook();
+                pictureBox1.BackColor = Color.Red;
+                button1.Text = "RUN";
+            }
+        }
+
+    }
+}
